2606df8933ac2175adafd62cc0d129f836473bdb
davidjjeong <jaewoong.jeong@duke.edu>
2023-11-26 13:54:40 +0900

    Added Welcome message in Dashboard main page

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,9 +1,13 @@
 import SideMenu from "./SideMenu";
+import styles from './page.module.css';
 
 export default function Dashboard() {
     return(
-        <>
+        <div>
             <SideMenu />
-        </>
+            <div className={styles.dashboardMain}>
+                <h1 className={styles.welcomeMessage}>Welcome</h1>
+            </div>
+        </div>
     );
 }
